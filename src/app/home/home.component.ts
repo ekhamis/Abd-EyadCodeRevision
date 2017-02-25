ee36c2ccbf8fb27d9e69c01530491de8ae7de0b4
ekhamis <ekhamis@gmail.com>
2017-02-25 14:45:33 +0100

    ts errors fixed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,30 +12,32 @@ export class HomeComponent /*implements OnInit - Eyad question: what is for?*/ {
         { name: 'GitHub', password: 'Git123', url: 'www.github.com' }
     ]
 
-    add(webN, webP, webL) {
+    add(webN: string, webP: string, webL: string) {
         if (webN === '' || webP === '' || webL === '') {
             alert('All fields must be completed')
         } else {
             let newUser = { name: webN, password: webP, url: webL }
-            if (webN && webP) this.data.push(newUser);
+            if (webN && webP) {
+                this.data.push(newUser)
+            };
             console.log(newUser)
             alert(newUser.name + ' was successfully added')
         }
     }
 
-    edit(Ename, Epass, Eurl) {
+    edit(Ename: string, Epass: string, Eurl: string) {
         if (Ename === '' || Eurl === '') {
             alert('All fields must be completed')
         }
         for (let x of this.data) {
             if (Ename === x.name && Eurl === x.url) {
                 if (Epass === '') {
-                    alert('Enter the new password')
+                    alert('Enter the new password');
                 } else {
                     x.password = Epass;
                     alert('Password Changed')
                     console.log('The new password is: ' + x.password);
-                    console.log(JSON.stringify(this.data, null, "  "));
+                    console.log(JSON.stringify(this.data, null, `  `));
                     // }
                 }
             }
@@ -43,21 +45,20 @@ export class HomeComponent /*implements OnInit - Eyad question: what is for?*/ {
     }
 
 
-    remove(deleteE) {
+    remove(deleteE: string) {
         if (deleteE === '') {
             alert('Which URl shall be removed?')
         }
         for (let i of this.data) {
             if (deleteE === i.url) {
-                let retVal = confirm("Are you sure?");
-                if (retVal == false) {
+                let retVal = confirm(`Are you sure?`);
+                if (retVal === false) {
                     return false;
                 } else {
                     this.data = this.data.filter(item => item.url !== deleteE);
-                    alert(deleteE + ' was sucssefully deleted')
-                    console.log(JSON.stringify(this.data, null, "  "));;
+                    alert(deleteE + ' was sucssesfully deleted');
+                    console.log(JSON.stringify(this.data, null, `  `));
                 }
-                // }
             }
         }
     }
@@ -72,7 +73,7 @@ export class HomeComponent /*implements OnInit - Eyad question: what is for?*/ {
 
         setTimeout(() => {
             x.style.display = 'none';
-        }, 7000)
+        }, 7000);
 
         // Eyad question: what are below functions used for?
         // constructor() { }
